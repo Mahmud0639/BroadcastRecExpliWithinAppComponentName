dcc651153751e0ce7b146a0ae3bf914779961a32
mahmud <bappimatubber1997@gmail.com>
2022-07-30 13:25:20 +0600

    This is also an explicit broadcast receiver example. But it is used component name to send broadcast.

---
+++ app/src/main/java/com/manuni/broadcastrecexpliwithinappcomponentname/MyBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.manuni.broadcastrecexpliwithinappcomponentname;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Broadcast message received.", Toast.LENGTH_SHORT).show();
+    }
+}
